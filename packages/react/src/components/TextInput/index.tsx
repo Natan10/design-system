6189ffdd354db049aaa5f359dfa2924daf342f87
Natan <natan-moreira1@hotmail.com>
2023-09-23 20:57:19 -0300

    fix: add ref to text input and adjust some styles

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,15 +1,18 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, forwardRef, ElementRef } from 'react'
 import { Input, Prefix, TextInpuContainer } from './styles'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
 }
 
-export function TextInput({ prefix, ...props }: TextInputProps) {
+export const TextInput = forwardRef<ElementRef<typeof Input>,TextInputProps>(({ prefix, ...props }: TextInputProps, ref) => {
   return (
     <TextInpuContainer>
       {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input {...props} />
+      <Input ref={ref} {...props} />
     </TextInpuContainer>
   )
 }
+)
+
+TextInput.displayName = 'TextInput'
